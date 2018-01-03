f7530bfe69eef3a10490e987674ff6879ea384cd
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2018-01-03 12:52:34 +0800

    Initial finished files

--- MiniImageGallery/ViewController.swift
+++ MiniImageGallery/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var label: UILabel!
+    
+    var imageInt = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,42 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func back(_ sender: Any) {
+        imageInt -= 1
+        self.imageGallery()
+    }
+    
+    @IBAction func next(_ sender: Any) {
+        
+        imageInt += 1
+        
+        self.imageGallery()
+        
+    }
+    
+    func imageGallery() {
+        label.text = String("\(imageInt)/6")
+        
+        imageView.image = UIImage(named: "Image\(imageInt)")
+        
+        if imageInt == 1 {
+            backButton.isEnabled = false
+            backButton.alpha = 0.25
+        }
+        else if imageInt == 6 {
+            nextButton.isEnabled = false
+            nextButton.alpha = 0.25
+        }
+        else {
+            backButton.isEnabled = true
+            nextButton.isEnabled = true
+            
+            nextButton.alpha = 1
+            backButton.alpha = 1
+        }
+    }
+    
 }
 
